b00d1b54ac4617840dca62a9e246223592d9d1fc
CreatiCoding <creaticoding@gmail.com>
2021-10-24 23:58:12 +0900

    feat: add Dockerfile and logging env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env);
   const port = process.env.PORT ?? '3000';
   await app.listen(process.env.PORT ?? '3000');
   console.log(`✅ Go to http://localhost:${port}/`);
