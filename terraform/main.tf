3c7fdeeefb30b2554e8ccf7ffe52f92bc7cd554e
CreatiCoding <seokho.jeong@toss.im>
2021-10-31 17:40:22 +0900

    chore: comment 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.region
 }
 
+variable "region" {
+  default = "ap-northeast-2"
+}
+
+// security group
+variable "service_ports" {
+  default = [22, 3000]
+}
 resource "aws_security_group" "web_security" {
   name        = "web_security"
   description = "web_security"
@@ -26,6 +34,15 @@ resource "aws_security_group" "web_security" {
   }
 }
 
+// EC2 key pair
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "generated_key" {
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+// EC2 Instance
 resource "aws_instance" "web" {
   # ami                    = "ami-0e4a9ad2eb120e054"
   ami                    = "ami-0b1f6dadd6c912cca"
@@ -66,26 +83,14 @@ resource "aws_instance" "web" {
   }
 }
 
-variable "service_ports" {
-  default = [22, 3000]
-}
-variable "region" {
-  default = "ap-northeast-2"
-}
-resource "tls_private_key" "my_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "aws_key_pair" "generated_key" {
-  public_key = tls_private_key.my_key.public_key_openssh
-}
-
-
+// Route53 도메인 호스트 Zone 생성
 resource "aws_route53_zone" "primary" {
   name = "creco-aws.com"
 }
 
-resource "aws_route53_record" "www" {
+// Route53 도메인 record 생성 (pc.creco-aws.com)
+// TODO: pc.creco-aws.com는 환경변수로 분리 필요
+resource "aws_route53_record" "web" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "pc.creco-aws.com"
   type    = "A"
@@ -93,25 +98,18 @@ resource "aws_route53_record" "www" {
   records = [aws_instance.web.public_ip]
 }
 
-output "welcome_to_my_web" {
-  value = "http://pc.creco-aws.com:3000"
-  # value = "http://${aws_instance.web.public_ip}:3000"
-}
-output "route53-NS" {
-  value       = " Name=${join(" Name=", aws_route53_zone.primary.name_servers)}"
-  description = "Fully-qualified domain name"
-}
-
-
+// 도메인 소유 Account 의 key 환경변수
 variable "DNS_AWS_ACCESS_KEY_ID" {
   type = string
 }
 variable "DNS_AWS_SECRET_ACCESS_KEY" {
   type = string
 }
+
+// 도메인 소유한 Account의 도메인의 Name Server를 새로 생성한 Host Zone으로 업데이트
 resource "null_resource" "modify_domain_name_servers" {
   provisioner "local-exec" {
-    command     = "chmod +x ./dns-register.sh && bash ./dns-register.sh >> dns-register.log"
+    command     = "chmod +x ./dns-register.sh && bash ./dns-register.sh"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       AWS_ACCESS_KEY_ID     = var.DNS_AWS_ACCESS_KEY_ID
@@ -122,4 +120,11 @@ resource "null_resource" "modify_domain_name_servers" {
       NAME_SERVER_3         = aws_route53_zone.primary.name_servers[3]
     }
   }
+}
+
+
+// 접속 경로 output
+output "welcome_to_my_web" {
+  value = "http://pc.creco-aws.com:3000"
+  # value = "http://${aws_instance.web.public_ip}:3000"
 }
